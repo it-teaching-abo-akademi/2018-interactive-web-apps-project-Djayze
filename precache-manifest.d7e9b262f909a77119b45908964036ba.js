412092dc3041f72bc5ddad74dce96806fbe6ce34
Djayze <jayzebarrier@gmail.com>
2019-01-22 15:52:33 +0100

    Updates

---
+++ precache-manifest.d7e9b262f909a77119b45908964036ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9121617f9a4cba924cf8",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Djayze.git/static/js/runtime~main.9121617f.js"
+  },
+  {
+    "revision": "c6c056d08d5f328854f9",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Djayze.git/static/js/main.c6c056d0.chunk.js"
+  },
+  {
+    "revision": "2366ba121e4d46df829b",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Djayze.git/static/js/1.2366ba12.chunk.js"
+  },
+  {
+    "revision": "c6c056d08d5f328854f9",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Djayze.git/static/css/main.7411cfd3.chunk.css"
+  },
+  {
+    "revision": "6c264476ba3af605896d7243b500b85c",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Djayze.git/index.html"
+  }
+];
